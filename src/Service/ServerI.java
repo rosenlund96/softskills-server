07d30f9b78e7835c9edf54160b9032b3b5d7cb98
sebastianhansen <sebastian.p.hansen@gmail.com>
2017-04-04 10:17:03 +0200

    Server interface og manager implementeret

--- src/Service/ServerI.java
+++ src/Service/ServerI.java
@@ -1,12 +1,24 @@
 package Service;
 
+import java.util.ArrayList;
+
 import javax.jws.WebMethod;
 import javax.jws.WebService;
+
+import Entities.AbstractItem;
+import Entities.Group;
+import Entities.User;
 
 @WebService
 public interface ServerI {
 
 	
 	@WebMethod boolean login(String bruger, String adgangskode) throws Exception;
+	@WebMethod boolean createGroup(String name, ArrayList<User> users, int size) throws Exception;
+	@WebMethod boolean getGroup(String name) throws Exception;
+	@WebMethod boolean deleteGroup(String name) throws Exception;
+	@WebMethod boolean getAllUsers() throws Exception;
+	@WebMethod boolean addProductToUser(AbstractItem item, User user, int qty) throws Exception;
+	@WebMethod boolean addToResults(User user,AbstractItem item) throws Exception;
 	
 }
--- src/Service/Servermanager.java
+++ src/Service/Servermanager.java
@@ -1,6 +1,7 @@
 package Service;
 
 import java.net.URL;
+import java.util.ArrayList;
 
 import javax.jws.WebService;
 import javax.xml.namespace.QName;
@@ -8,6 +9,8 @@ import javax.xml.ws.Service;
 
 import Controllers.Groupmanager;
 import Controllers.Usermanager;
+import Entities.AbstractItem;
+import Entities.Group;
 import Entities.User;
 import Brugerautorisation.data.Bruger;
 import Brugerautorisation.transport.Brugeradmin;
@@ -17,10 +20,13 @@ import Brugerautorisation.transport.Brugeradmin;
 public class Servermanager {
 
 	Bruger b = null;
+	User current = null;
+	public Groupmanager aManager;
+	public Usermanager manager;
 	
 	public Servermanager(){
-		Usermanager manager = new Usermanager();
-		Groupmanager gManager = new Groupmanager(manager);
+		manager = new Usermanager();
+		this.aManager = new Groupmanager(manager);
 		
 	}
 	
@@ -35,13 +41,35 @@ public class Servermanager {
 } catch (Throwable e) {
     return null;
 }
-    User current = new User(b.getName(),b.getLastname(),b.getMail(),b.getRetning());
+    current = new User(b.getName(),b.getLastname(),b.getMail(),b.getRetning());
     
     return current;
 }
 	
-	//All methods from Usermanager//
-	
 	//All methods from Groupmanager//
+	public void createGroup(String name, ArrayList<User> users, int size) {
+		aManager.createGroup(name, users, size);
+	}
+	
+	public Group getGroup(String name){
+		return aManager.getGroup(name);
+	}
+	
+	public Boolean deleteGroup(String name){
+		return aManager.deleteGroup(name);
+	}
+	
+	//All methods from Usermanager//
+	public ArrayList<User> getAllUsers(){
+		return manager.getAllUsers();
+	}
+	
+	public void addProductToUser(AbstractItem item, User user, int qty){
+		 manager.addProductToUser(item, user, qty);
+	}
+	
+	public void addToResults(User user,AbstractItem item) {
+		manager.addToResults(user, item);
+	}
 	
 }
